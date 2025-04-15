4f1577f4bbe49c23fd5a6a8640331315b4659de1
VictorCezar212 <victorcesar212@gmail.com>
2025-04-15 19:22:11 -0300

    projeo

--- src/index.js
+++ src/index.js
@@ -6,29 +6,46 @@ const rl = readline.createInterface({
 });
 
 rl.question("Digite o nome do herói: ", (nome) => {
-  rl.question("Digite a quantidade de XP do herói: ", (xpStr) => {
-    let xp = parseInt(xpStr);
-    let nivel = "";
+  let xp;
+  let xpValido = false;
 
+  const perguntarXP = () => {
+    rl.question("Digite a quantidade de XP do herói: ", (xpStr) => {
+      xp = parseInt(xpStr);
+
+      if (!isNaN(xp)) {
+        xpValido = true;
+        definirNivel(xp, nome);
+      } else {
+        console.log("Valor inválido! Digite um número para XP.");
+        perguntarXP();
+      }
+    });
+  };
+
+  const definirNivel = (xp, nome) => {
+    let nivel;
     if (xp < 1000) {
       nivel = "Ferro";
-    } else if (xp >= 1001 && xp <= 2000) {
+    } else if (xp <= 2000) {
       nivel = "Bronze";
-    } else if (xp >= 2001 && xp <= 5000) {
+    } else if (xp <= 5000) {
       nivel = "Prata";
-    } else if (xp >= 5001 && xp <= 7000) {
+    } else if (xp <= 7000) {
       nivel = "Ouro";
-    } else if (xp >= 7001 && xp <= 8000) {
+    } else if (xp <= 8000) {
       nivel = "Platina";
-    } else if (xp >= 8001 && xp <= 9000) {
+    } else if (xp <= 9000) {
       nivel = "Ascendente";
-    } else if (xp >= 9001 && xp <= 10000) {
+    } else if (xp <= 10000) {
       nivel = "Imortal";
-    } else if (xp >= 10001) {
+    } else {
       nivel = "Radiante";
     }
 
     console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
     rl.close();
-  });
+  };
+
+  perguntarXP();
 });
